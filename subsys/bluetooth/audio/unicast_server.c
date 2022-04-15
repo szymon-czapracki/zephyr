54300018f1c42533317fec1c0e9427c183464724
Emil Gydesen <emil.gydesen@nordicsemi.no>
2022-04-15 13:48:16 +0200

    Bluetooth: Audio: Add unicast server stream disable
    Add support for disabling streams as the unicast
    server.
    
    Signed-off-by: Emil Gydesen <emil.gydesen@nordicsemi.no>

--- subsys/bluetooth/audio/unicast_server.c
+++ subsys/bluetooth/audio/unicast_server.c
@@ -11,8 +11,7 @@
 #include <zephyr/bluetooth/audio/audio.h>
 
 #include "pacs_internal.h"
-#include "stream.h"
-#include "ascs_internal.h"
+#include "endpoint.h"
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_AUDIO_DEBUG_UNICAST_SERVER)
 #define LOG_MODULE_NAME bt_unicast_server
@@ -64,6 +63,35 @@ int bt_audio_unicast_server_location_changed(enum bt_audio_dir dir)
 int bt_audio_unicast_server_available_contexts_changed(void)
 {
 	return bt_pacs_available_contexts_changed();
+}
+
+int bt_unicast_server_disable(struct bt_audio_stream *stream)
+{
+	struct bt_audio_ep *ep;
+	int err;
+
+	if (unicast_server_cb != NULL && unicast_server_cb->disable != NULL) {
+		err = unicast_server_cb->disable(stream);
+	} else {
+		err = -ENOTSUP;
+	}
+
+	if (err != 0) {
+		return err;
+	}
+
+	ep = stream->ep;
+
+	/* The ASE state machine goes into different states from this operation
+	 * based on whether it is a source or a sink ASE.
+	 */
+	if (ep->dir == BT_AUDIO_DIR_SOURCE) {
+		ascs_ep_set_state(ep, BT_AUDIO_EP_STATE_DISABLING);
+	} else {
+		ascs_ep_set_state(ep, BT_AUDIO_EP_STATE_QOS_CONFIGURED);
+	}
+
+	return 0;
 }
 
 int bt_unicast_server_release(struct bt_audio_stream *stream, bool cache)
